95b257c8f3603bfe0ee623a7b2a24bd9ae5b180d
Tanya Nevskaya <unparalloser@gmail.com>
2021-12-02 23:28:48 +0300

    Day 1: Sonar Sweep

---
+++ src/day1.clj
@@ -0,0 +1,22 @@
+(ns day1
+  (:require [clojure.java.io :as io]))
+
+(def data
+  (with-open [r (io/reader (io/resource "day1_input.txt"))]
+    (mapv #(Integer/parseInt %) (line-seq r))))
+
+(defn count-inc
+  ([nums] 
+   (count-inc nums 0))
+  ([[h & [n & _ :as t]] acc]
+   (if (empty? t)
+     acc
+     (if (< h n)
+       (recur t (inc acc))
+       (recur t acc)))))
+
+(def sliding-window-sums
+  (map (partial reduce +) (partition 3 1 data)))
+
+(println (count-inc data)) ; part 1
+(println (count-inc sliding-window-sums)) ; part 2
